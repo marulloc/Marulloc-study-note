5d3e5d196ce750aac022761e77df0d1a82416f0c
marulloc <marullo@naver.com>
2021-03-18 09:47:05 +0900

    [Python] 파일명 정렬 - 정규표현식 & 정렬

---
+++ Python/정렬/파일명 정렬.py
@@ -0,0 +1,20 @@
+# 프로그래머스 카카오 블라인드 파일명 정렬
+# https://programmers.co.kr/learn/courses/30/lessons/17686#
+
+import re
+p = re.compile("([a-zA-Z .\-]+)([0-9]+)([0-9a-zA-Z .\-]*)")
+
+def solution(files):
+    # 정규표현식 사용
+    tokens = []
+    idx = 0
+    for file in files:
+        match = p.findall(file)[0]
+        tokens.append([match[0],match[1],match[2]])
+        idx += 1
+    
+    
+    # 정렬 lambda에 튜플로
+    tokens.sort(key = lambda x : (x[0].lower(), int(x[1])))
+    answer = [''.join(token) for token in tokens]
+    return answer
